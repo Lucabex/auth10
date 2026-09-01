e6a0c47df988ee3c2415d5745a18e76e6307fc19
Luca Bercioux <Lucabex@gmail.com>
2026-09-01 13:54:47 +0100

    Fix migration issue

--- Migrations/20260831211401_correcttypo.cs
+++ Migrations/20260831211401_correcttypo.cs
@@ -6,7 +6,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace auth10.Migrations
 {
     /// <inheritdoc />
-    public partial class initial : Migration
+    public partial class correcttypo : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
